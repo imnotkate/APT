b0077df2a4176fe1e6777b3533c5a43951d2ef75
imnotkate <k.rankin07@outlook.com>
2024-02-20 16:17:04 +0000

    login and register update

--- constants/Theme.js
+++ constants/Theme.js
@@ -4,11 +4,13 @@ const {width, height} = Dimensions.get('window');
 
 export const colors = {
   primary: '#070f18',
-  gray: '#8b8989',
+  gray: '#ececec',
   lightGray: '#b2b2b2',
   light: '#fbfbfb',
   white: '#fff',
   black: '#000',
+  blue: '#0c1f41',
+  red: '#de1d35'
 };
 
 export const shadow = {
--- screens/Register.js
+++ screens/Register.js
@@ -10,18 +10,20 @@ export default function Login() {
       <View className="bg-white h-full w-full">
         <StatusBar style='light' />
        
-      <View className="h-full w-full flex justify-around pt-40 pb-10">
+      <View className="h-50% w-full flex justify-around pt-40 pb-10">
         <View className="flex items-center">
-          <Text className="text-black font-bold tracking-wider text-5xl">Sign Up</Text>
-      </View>
+        <Image
+        source={require('../assets/images/APT.png')} // Replace with the actual path to your image
+        style={{ width: 400, height: 400 }} />      
+        </View>
 
-     <View className="flex items-center mx-4 space-y-4">
-        <View className="bg-black/5 p-5 rounded-2x1 w-full nb-3">
-            <TextInput placeholder='Username' placeholderTextColor={'gray'} />
+     <View className="flex items-center mx-5 space-y-4">
+      <View className="bg-black/5 p-5 rounded-2x1 w-full nb-3">
+            <TextInput placeholder='Username' placeholderTextColor={'gray'}/>
         </View>
 
         <View className="bg-black/5 p-5 rounded-2x1 w-full nb-3">
-            <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+            <TextInput placeholder='Email' placeholderTextColor={'gray'}/>
         </View>
 
         <View className="bg-black/5 p-5 rounded-2x1 w-full nb-3">
@@ -29,7 +31,7 @@ export default function Login() {
         </View>
 
         <View className="w-full">
-          <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl nb-3">
+          <TouchableOpacity className="w-full p-3 bg-blue-900 rounded-2xl nb-3" >
             <Text className="text-xl font-bold text-white text-center">Register</Text>
           </TouchableOpacity>
         </View>
@@ -37,7 +39,7 @@ export default function Login() {
         <View className="flex-row justify-center">
             <Text>Already have an account? </Text>
             <TouchableOpacity onPress={() => navigation.push('Login')}>
-                <Text className="text-sky-600">Log In</Text>
+                <Text className="text-blue-900">Log In</Text>
             </TouchableOpacity>
         </View>
 
