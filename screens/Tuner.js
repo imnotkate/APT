adaad739a23f1c4e8fc19f54ff19c511d314741f
imnotkate <k.rankin07@outlook.com>
2024-02-27 16:44:56 +0000

    tuner update

--- screens/Tuner.js
+++ screens/Tuner.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
 import { Image, Text, View, TouchableOpacity, TextInput, Button, StyleSheet, Switch } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import axios from 'axios';
-import { Iconoir } from 'iconoir-react-native';
+import { NavArrowDown, NavArrowRight } from 'iconoir-react-native';
 
 const Tuner = () => {
-  const [selectedTuning, setTuning] = useState('EStandard');
+  const [selectedTuning, setTuning] = useState('E Standard');
   const [selectedString, setSelectedString] = useState('E2');
   const [tuningProgress, setTuningProgress] = useState(0);
-  const [selectedGuitar, setGuitar] = useState('6string');
+  const [selectedGuitar, setGuitar] = useState('Guitar 6-string');
+  const [selectedInstrument, setInstrument] = useState('Guitar');
 
-  const guitars = ['6string', '7string', '8string', '12string'];
-  const tunings = ['EStandard', 'DropD', 'OpenD', 'DropC', 'OpenC', 'OpenG', 'DropB', 'OpenE', 'DropA']; 
+  const instruments = ['Guitar', 'Bass', 'Soprano Ukulele', 'Concert Ukulele', 'Tenor Ukulele', 'Baritone Ukulele', 'Banjo', 'Mandolin', 'Sitar'];
+  const guitars = ['Guitar 6-string', 'Guitar 7-string', 'Guitar 8-string', 'Guitar 12-string'];
+  const tunings = ['E Standard', 'Drop D', 'Open D', 'Drop C', 'Open C', 'Open G', 'Drop B', 'Open E', 'Drop A']; 
 
   const stringsData = {
-    EStandard: ['E2', 'A', 'D', 'G', 'B', 'E4'], 
-    DropD: ['D2', 'A', 'D3', 'G', 'B', 'E'], 
-    OpenD: ['D2', 'A', 'D3', 'F#', 'A', 'D4'],
-    DropC: ['C2', 'G', 'C3', 'F', 'A', 'D'],
-    OpenC: ['C2', 'G2', 'C3', 'G3', 'C4', 'E'],
-    OpenG: ['D2', 'G2', 'D3', 'G3', 'B', 'D4'],
-    DropB: ['B1', 'Gb', 'B2', 'E', 'Ab', 'Db'],
-    OpenE: ['E2', 'B2', 'E3', 'G#', 'B3', 'E4'],
-    DropA: ['A1', 'E', 'A2', 'D', 'F#', 'B'],
+    'E Standard': ['E2', 'A', 'D', 'G', 'B', 'E4'], 
+    'Drop D': ['D2', 'A', 'D3', 'G', 'B', 'E'], 
+    'Open D': ['D2', 'A', 'D3', 'F#', 'A', 'D4'],
+    'Drop C': ['C2', 'G', 'C3', 'F', 'A', 'D'],
+    'Open C': ['C2', 'G2', 'C3', 'G3', 'C4', 'E'],
+    'Open G': ['D2', 'G2', 'D3', 'G3', 'B', 'D4'],
+    'Drop B': ['B1', 'Gb', 'B2', 'E', 'Ab', 'Db'],
+    'Open E': ['E2', 'B2', 'E3', 'G#', 'B3', 'E4'],
+    'Drop A': ['A1', 'E', 'A2', 'D', 'F#', 'B'],
   };
   const strings = stringsData[selectedTuning] || []; // Get strings based on selected tuning
-
  
   const handleTuningChange = (value) => {
     setTuning(value);
+    setTuningPickerVisible(false);
   };
 
   const handleTuningProgress = () => {
@@ -41,7 +42,18 @@ const Tuner = () => {
 
   const handleGuitarChange = (value) => {
     setGuitar(value);
+    setGuitarPickerVisible(false);
   }
+
+  const handleInstrumentChange = (value) => {
+    setInstrument(value);
+  }
+
+  const [auto, setAuto] = useState(false);
+
+  const handleToggleSwitch = () => {
+    setAuto(!auto);
+  };
 
    // connection to flask webserver 
   // send msg
@@ -49,23 +61,15 @@ const Tuner = () => {
     const messageData = {
       message: string
     };
-
     axios.post("http://192.168.231.3:5000/tune_string", messageData).then(response => {console.log('msg sent', response.data);}).catch(error => {console.error('error', error);});
   };
 
+  const pickerTextStyle = {
+    fontSize: 20, // Adjust the font size as needed
+    textAlign: 'center', // Center the text
+  };
+
   const styles = StyleSheet.create({
-    container: {
-      flex: 1, // Use flex to take up the full screen
-      justifyContent: 'flex-start', // Align children to the start vertically
-      alignItems: 'flex-end', // Align children to the end horizontally
-      top:20
-    },
-    tuningSetting: {
-      flexDirection: 'row', // Layout children in a row
-      alignItems: 'center', // Center children vertically in the row
-      marginTop: -15, // Adjust top spacing to match your needs, considering status bar height
-      marginRight: 30, // Adjust right spacing to match your needs
-    },
     text: {
       fontSize: 22, // 'text-lg' equivalent
       paddingRight: 8, // 'pr-2' equivalent
@@ -73,15 +77,6 @@ const Tuner = () => {
       alignContent: 'center', // Center text horizontally
       alignItems: 'center',
     },
-    pickerContainer: {
-      padding: 6, // 'pt-1.5' equivalent
-      borderColor: 'red',
-      borderWidth: 1,
-      padding: 10,
-      width: 90,
-      borderRadius: 25
-    },
-
     stringContainer: {
       // Styles for the string container
       flexDirection: 'row',
@@ -105,58 +100,67 @@ const Tuner = () => {
     }
   });
 
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
+  const [isGuitarPickerVisible, setGuitarPickerVisible] = useState(false);
+  const [isTuningPickerVisible, setTuningPickerVisible] = useState(false);
 
   return (
     <View className="bg-white h-full w-full bg-white">
 
-    <View className='bg-white pt-20 items-center'>
-      <Image
-        source={require('../assets/images/logosmall.png')} 
-        style={{ width: 130, height: 70, position: 'absolute',  top: 80, left: 30}} />  
-    </View> 
+<View style={{ backgroundColor: 'white', paddingTop: 70, flexDirection: 'row', alignItems: 'center', paddingLeft: 10 }}>
+  <Image
+    source={require('../assets/images/logosmall.png')}
+    style={{ width: 130, height: 70, marginLeft: 20}}
+  />
+  <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 'auto', paddingRight: 20}}>
+    <Text style={{ fontSize: 18, marginRight: 10 }}>AUTO</Text>
+    <Switch
+      value={auto}
+      onValueChange={handleToggleSwitch}
+    />
+  </View>
+</View>
 
+<View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', flexDirection: 'column', paddingLeft: 10 }}>
+  <TouchableOpacity
+    style={{
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      borderRadius: 25,
+      fontSize: 20,
+      width: 190,
+    }}
+    onPress={() => {}}
+  >
+    <Text style={{fontSize: 20}}>{selectedGuitar}</Text>
+    <NavArrowRight color="black" height={25} width={32} style={{ }} />
+  </TouchableOpacity>
 
-    <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'flex-end'}}>
-          <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 20, marginRight: 30,}}>
-            <View style={{padding: 6, borderColor: 'red', borderWidth: 1, padding: 10, width: 90,borderRadius: 25}}>
-              <RNPickerSelect
-                onValueChange={handleGuitarChange}
-                items={guitars.map(type => ({ label: type, value: type }))}
-                value={selectedGuitar}
-              />
-            </View>
-          </View>
-    </View>
+  {/* <TouchableOpacity
+    style={{
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 25,
+      fontSize: 20,
+      width: 190,
+    
+    }}
+    onPress={() => {}}
+  >
+    <RNPickerSelect
+      onValueChange={handleTuningChange}
+      items={tunings.map(type => ({ label: type, value: type }))}
+      value={selectedTuning}
+      placeholder={{ label: 'Select Tuning', value: null }}
+      style={{ inputAndroid: pickerTextStyle, inputIOS: pickerTextStyle }}
+    />
+    <NavArrowDown color="black" height={25} width={32} style={{ marginLeft: 10 }} />
+  </TouchableOpacity> */}
+</View>
 
-    <View style={styles.container}>
-          <View style={styles.tuningSetting}>
-            <Text style={styles.text}>Tuning:</Text>
-            <View style={styles.pickerContainer}>
-              <RNPickerSelect
-                onValueChange={handleTuningChange}
-                items={tunings.map(type => ({ label: type, value: type }))}
-                value={selectedTuning}
-              />
-            </View>
-          </View>
-    </View>
-
-    <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left'}}>
-      <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 50 }}>
-      <Text style={{fontSize: 18, paddingLeft: 40, paddingRight: 10}}>AUTO</Text>
-        <Switch
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
-      </View>
-    </View>
-
-        
-
-  <View style={{flexDirection: 'row', alignItems: 'center', paddingTop: 40}}>
+  <View style={{flexDirection: 'row', alignItems: 'center', paddingTop: 20}}>
         {/* Container for the string circles */}
         <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingBottom: 100, paddingLeft: 40 }}>
           {strings.map((string, index) => (
