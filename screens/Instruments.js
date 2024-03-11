2bec73e163c7f98eb7a842a9f52bce3ee0691e03
imnotkate <k.rankin07@outlook.com>
2024-03-11 22:51:44 +0000

    fix

--- screens/Instruments.js
+++ screens/Instruments.js
@@ -17,7 +17,7 @@ function Instruments() {
     navigation.getState().params?.selectedInstrument || 'Guitar 6-string'
   );
 
-  const navigateToTuner = (selectedHead) => {
+  const navigateToTuner = (selectedHead, selectedInstrument) => {
     navigation.navigate('Tune', { selectedHead, selectedInstrument });
   };
 
